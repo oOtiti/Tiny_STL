079d1ca00f4962b45a55f08b2f1daaac49cad5cd
sll00 <1275142234@qq.com>
2025-08-10 21:53:51 +0800

    项目目的和介绍

---
+++ README.md
@@ -0,0 +1,6 @@
+### 简介
+该项目是对https://github.com/Alinshans/MyTinySTL的复现。
+
+
+小白练手git、cmake和项目形成
+
---
+++ STL/mytype_traits.h
@@ -0,0 +1,8 @@
+#ifndef my_type_traits_H_
+#define my_type_traits_H_
+
+// 这个头文件用于提取类型信息
+
+#include <type_traits>
+
+#endif
