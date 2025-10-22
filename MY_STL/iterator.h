79ecb0b051ae8e7e3ab317f597b5e092db3a5a27
sll00 <1275142234@qq.com>
2025-10-22 20:09:39 +0800

    更加规范化

---
+++ MY_STL/iterator.h
@@ -0,0 +1,15 @@
+#ifndef MY_iterator_H_ 
+#define MY_iterator_H_ 1
+
+// 这个头文件用于迭代器的设计
+#include <iterator>
+#include <cstddef>//调用库函数来设计
+#include "type_traits.h"
+namespace mystl
+{
+    template<typename T>
+    struct nput_Iterato r
+} // namespace mystl
+
+
+#endif
--- MY_STL/type_traits.h
+++ MY_STL/type_traits.h
@@ -6,44 +6,49 @@
 #include <type_traits> //调用库函数来辅助判断（编译时期的函数）
 namespace mystl
 {
-    template <typename T,T _v>
+    template <class T,T _v>
     struct m_integral_constant
     {
         static constexpr T value=_v;
+        using value_type = T;
+    using type = integral_constant<T, _v>;
+    // constexpr operator value_type() const noexcept { return value; }
+    // constexpr value_type operator()() const noexcept { return value; }
     };
     
     //模板特化(特例化)
     template<bool b>
-    using m_bool_constant =m_integral_constant<bool,b>;
+        using m_bool_constant =m_integral_constant<bool,b>;
 
-    m_bool_constant<true>  m_true_type;
-    m_bool_constant<false> m_false_type;
+    //定义新类型，方便继承
+    using m_true_type  = m_bool_constant<true>;
+    using m_false_type = m_bool_constant<false>;
     
     
     
     //经典的type traits的实例
-    template <typename T1, typename T2>
+    template <class T1, class T2>
     struct pair;
 
-    template <typename T>
+    template <class T>
     struct is_pair : mystl::m_false_type {};
 
-    template <typename T1, typename T2>
-    struct is_pair<mystl::pair<T1,T2>> : mystl::m_true_type{};
+    template <class T1, class T2>
+    struct is_pair<mystl::pair<T1,T2>> : mystl::m_true_type {};
 
     //仿写
-    template<typename T1, typename T2, typename T3>
+    template<class T1, class T2, class T3>
     struct triplet;
 
-    template<typename T>
+    template<class T>
     struct is_triplet : mystl::m_false_type {};
 
-    template<typename T1,typename T2>
+    template<class T1,class T2>
     struct is_triplet : mystl::m_false_type {};
 
-    template<typename T1, typename T2, typename T3>
+    template<class T1, class T2, class T3>
     struct is_triplet<mystl::triplet<T1,T2,T3>> :m_true_type {};
     
-} // namespace mystl
+}
 
 #endif
